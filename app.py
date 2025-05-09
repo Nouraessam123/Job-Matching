968d940afccd2af7d3ded93b8b0ec78230109374
Nouraessam123 <138504609+Nouraessam123@users.noreply.github.com>
2025-05-09 16:57:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -48,4 +48,6 @@ def job_api():
     return jsonify({"jobs": jobs})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
